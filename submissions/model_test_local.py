ce33fb85aa36d752129fa9ea3c83f9870821909d
ThomBordes <thomas.bordes@onevisage.com>
2023-12-05 15:34:41 +0100

    Merge all data done / use Training jupyter notebook to preprocess, train and predict

--- submissions/model_test_local.py
+++ submissions/model_test_local.py
@@ -1,50 +1,18 @@
-import os
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import TimeSeriesSplit
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import mean_squared_error
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import Ridge
+from sklearn.pipeline import make_pipeline
+import xgboost as xgb
+from sklearn.preprocessing import StandardScaler
 import holidays
-
-def get_train_data(path="."):
-    f_name = "train.parquet"
-    return _read_original_data(path, f_name)
-
-
-def get_test_data(path="."):
-    f_name = "test.parquet"
-    return _read_original_data(path, f_name)
-
-def get_final_test_data(path="."):
-    f_name = "final_test.parquet"
-    data = pd.read_parquet(os.path.join(path, "data", f_name))
-    # Sort data by date and counter name for correct cross-validation
-    data = data.sort_values(["date", "counter_name"])
-    return data
-    
-def read_original_data():
-
-    X_train, y_train = get_train_data()
-    X_test, y_test = get_test_data()
-
-    return X_train, y_train, X_test, y_test
-    
-def _read_original_data(path, f_name):
-    """
-    Reads and preprocesses data from a parquet file.
-    Input:
-    - path: Directory path where data file is located
-    - f_name: Name of the data file
-    Output:
-    - X_df: Processed feature DataFrame
-    - y_array: Array of target variable values
-    """
-    _target_column_name = "log_bike_count"
-    data = pd.read_parquet(os.path.join(path, "data", f_name))
-    # Sort data by date and counter name for correct cross-validation
-    data = data.sort_values(["date", "counter_name"])
-    y_array = data[_target_column_name].values
-    X_df = data.drop([_target_column_name, "bike_count"], axis=1)
-    return X_df, y_array
 
 
 def _merge_external_data_weather(X):
@@ -56,7 +24,8 @@ def _merge_external_data_weather(X):
     - X: Main DataFrame with merged weather data
     """
     
-    file_path = "data/external_data.csv"
+    #file_path = "../input/additional-data/data/external_data.csv"
+    file_path = "../data/external_data.csv"
     df_ext = pd.read_csv(file_path, parse_dates=["date"])
     df_ext['date'] = pd.to_datetime(df_ext['date']).astype('datetime64[us]')
     
@@ -185,7 +154,9 @@ def _merge_indicators_COVID(X):
     - X: DataFrame with merged COVID indicators
     """
     
-    file_path = "data/table-indicateurs-open-data-dep-2023-COVID.csv"
+    #file_path = "../input/additional-data/data/table-indicateurs-open-data-dep-2023-COVID.csv"
+    file_path = "../data/table-indicateurs-open-data-dep-2023-COVID.csv"
+    
     columns_COVID = ["date", "lib_dep", "hosp", "rea", "incid_rea", "rad"] 
     df_ext = pd.read_csv(file_path, usecols = columns_COVID, parse_dates=["date"])
     df_ext['date'] = pd.to_datetime(df_ext['date']).astype('datetime64[us]')
@@ -239,9 +210,12 @@ def _merge_road_accidents_by_year(year, start_date, end_date):
     columns_VEHICULES = ["Num_Acc", "catv"]
 
     # Read data for CARACTERISTIQUES, USAGERS, and VEHICULES
-    data_CARACT_path = f"data/road accident/caracteristiques-{year}.csv"
-    data_USAGERS_path = f"data/road accident/usagers-{year}.csv"
-    data_VEHICULES_path = f"data/road accident/vehicules-{year}.csv"
+    # data_CARACT_path = f"../input/additional-data/data/road accident/caracteristiques-{year}.csv"
+    # data_USAGERS_path = f"../input/additional-data/data/road accident/usagers-{year}.csv"
+    # data_VEHICULES_path = f"../input/additional-data/data/road accident/vehicules-{year}.csv"
+    data_CARACT_path = f"../data/road accident/caracteristiques-{year}.csv"
+    data_USAGERS_path = f"../data/road accident/usagers-{year}.csv"
+    data_VEHICULES_path = f"../data/road accident/vehicules-{year}.csv"
     data_CARACT = pd.read_csv(data_CARACT_path, sep=";", usecols=columns_CARACT)
     data_USAGERS = pd.read_csv(data_USAGERS_path, sep=";", usecols=columns_USAGERS)
     data_VEHICULES = pd.read_csv(data_VEHICULES_path, sep=";", usecols=columns_VEHICULES)
@@ -309,3 +283,177 @@ def _merge_road_accidents(X):
     X = X.sort_values("orig_index")
     del X["orig_index"]
     return X
+
+def add_external_data(X):
+    
+    X = X.copy()
+    
+    # merge original data + external data
+    merged_X_train_external_DATA = _merge_external_data_weather(X)
+    
+    # merge original data + external data + holidays
+    merged_X_train_external_HOLIDAYS = _merge_holidays_week_end(merged_X_train_external_DATA)
+    
+    # merge original data + external data + holidays + data COVID
+    merged_X_train_external_HOLIDAYS_COVID = _merge_Curfews_lockdowns_COVID(merged_X_train_external_HOLIDAYS)
+    merged_X_train_external_HOLIDAYS_COVID = _merge_indicators_COVID(merged_X_train_external_HOLIDAYS_COVID)
+    
+    # merge original data + external data + holidays + data COVID + data accidents
+    merged_X_train_external_HOLIDAYS_COVID_ACCIDENTS = _merge_road_accidents(merged_X_train_external_HOLIDAYS_COVID)
+
+    return merged_X_train_external_HOLIDAYS_COVID_ACCIDENTS
+    
+def get_train_data(f_name):
+    _target_column_name = "log_bike_count"
+    data = pd.read_parquet(f_name)
+    # Sort by date first, so that time based cross-validation would produce correct results
+    data = data.sort_values(["date", "counter_name"])
+    y_array = data[_target_column_name].values
+    X_df = data.drop([_target_column_name, "bike_count"], axis=1)
+    return X_df, y_array
+
+
+
+def get_test_data():
+    #f_name = "../input/mdsb-2023/final_test.parquet"
+    f_name = "../data/final_test.parquet"
+    data = pd.read_parquet(f_name)
+    # Sort by date first, so that time based cross-validation would produce correct results
+    data = data.sort_values(["date", "counter_name"])
+    return data
+
+def read_data():
+
+    #X_train, y_train = get_train_data("../input/mdsb-2023/train.parquet")
+    X_train, y_train = get_train_data("../data/train.parquet")
+    X_test = get_test_data()
+    
+    return X_train, y_train, X_test
+
+
+def _encode_dates(X):
+    
+    '''
+    Splits the 'date' columns of the input DataFrame into several columns (year, month, day, weekday, hour)
+    
+    Parameters:
+        X (pd.DataFrame): the dataframe to modify
+    
+    Returns:
+        X (pd.DataFrame): the modified dataframe
+    '''
+    
+    # Duplicate X to work on it
+    X = X.copy()
+    
+    # Create new columns with date parts from X.date
+    X.loc[:, "year"] = X["date"].dt.year
+    X.loc[:, "month"] = X["date"].dt.month
+    X.loc[:, "day"] = X["date"].dt.day
+    X.loc[:, "weekday"] = X["date"].dt.weekday
+    X.loc[:, "hour"] = X["date"].dt.hour
+
+    # Adding cosinus and sinus features from date variables to enhance the date periodicity
+    X['cos_hour'] = np.cos(X['hour']*(2.*np.pi/24))
+    X['sin_hour'] = np.sin(X['hour']*(2.*np.pi/24))
+    X['cos_day'] = np.cos(X['day']*(2.*np.pi/30))
+    X['sin_day'] = np.sin(X['day']*(2.*np.pi/30))
+    X['cos_month'] = np.cos(X['month']*(2.*np.pi/12))
+    X['sin_month'] = np.sin(X['month']*(2.*np.pi/12))
+    X['cos_weekday'] = np.cos(X['weekday']*(2.*np.pi/7))
+    X['sin_weekday'] = np.sin(X['weekday']*(2.*np.pi/7))
+    
+    # Clean the new dataframe and return it
+    X.drop(columns=["date"], inplace=True)
+    X.drop(columns=["year", 'month', 'day', 'weekday', 'hour'], inplace=True)
+    return X
+    
+def preprocessing(X_train):
+    
+    date_encoder = FunctionTransformer(_encode_dates)
+    date_cols = _encode_dates(X_train[["date"]]).columns.tolist()
+
+    categorical_encoder = OneHotEncoder(handle_unknown="ignore")
+    categorical_cols = ["counter_name", "site_name"]
+
+    numeric_encoder = StandardScaler()
+    numeric_cols = ['latitude', 'longitude', 't', 'ff', 'u', 'ssfrai', 'n', 'vv', 'rr3', 'hosp', 'rea', 'incid_rea', 'rad', 'Count_accidents']
+    #numeric_cols = ['t', 'ff', 'u', 'ssfrai', 'n', 'vv', 'rr3']
+    
+    preprocessor = ColumnTransformer(
+        [
+            ("date", OneHotEncoder(handle_unknown="ignore"), date_cols),
+            ("cat", categorical_encoder, categorical_cols),
+            ("num", numeric_encoder, numeric_cols),
+        ],
+        remainder="passthrough"  # This will pass through other columns not specified
+    )
+    return preprocessor, date_encoder
+
+    
+def train_model(X_train, y_train, preprocessor, date_encoder):
+    
+    best_params = {'colsample_bytree': 0.6154469128110744,
+                  'gamma': 1,
+                  'learning_rate': 0.09803049874792026,
+                  'max_depth': 9,
+                  'n_estimators': 363,
+                  'subsample': 0.5171942605576092}
+    
+    regressor = xgb.XGBRegressor(objective='reg:squarederror',
+                        n_estimators=best_params['n_estimators'],
+                        max_depth=best_params['max_depth'],
+                        learning_rate=best_params['learning_rate'],
+                        subsample=best_params['subsample'],
+                        colsample_bytree=best_params['colsample_bytree'],
+                        gamma=best_params['gamma'],
+                        seed=42
+                )
+    
+    pipe_XGBRegressor = make_pipeline(date_encoder, preprocessor, regressor)
+    pipe_XGBRegressor.fit(X_train, y_train)
+    
+    return pipe_XGBRegressor 
+
+    
+def submission_kaggle(pipe, X_test):
+    y_pred = pipe.predict(X_test)
+    print(y_pred)
+    results = pd.DataFrame(
+        dict(
+            Id=np.arange(y_pred.shape[0]),
+            log_bike_count=y_pred,
+        )
+    )
+    results.to_csv("submission.csv", index=False)
+    
+
+def main():
+
+    # Read data for training
+    X_train, y_train, X_test = read_data()
+    X_train_plus = add_external_data(X_train)
+
+    # Read data for testing
+    X_test_plus = add_external_data(X_test)
+    
+    # Apply feature engineering
+    X_train_plus_FI = X_train_plus.drop(["counter_id", "counter_technical_id", "counter_installation_date"], axis = 1)
+    X_test_plus_FI = X_test_plus.drop(["counter_id", "counter_technical_id", "counter_installation_date"], axis = 1)
+
+    
+
+    print(X_train_plus_FI)
+    
+    # Get preprocessor
+    preprocessor, date_encoder = preprocessing(X_train_plus_FI)
+    
+    # train_model
+    pipe = train_model(X_train_plus_FI, y_train, preprocessor, date_encoder)
+
+    # Get submission kaggle to csv
+    submission_kaggle(pipe, X_test_plus_FI)
+    
+    
+if __name__ == "__main__":
+    main()
